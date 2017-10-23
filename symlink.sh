81c52bb3eeceec16a74d98f7bf7e2859b30a68fd
Neal Marquez <nmmarquez@gmail.com>
2017-10-23 08:03:50 -0700

    update symlink to bloacks config

--- symlink.sh
+++ symlink.sh
@@ -9,6 +9,8 @@ ln -s $FOLDER/.zshrc ~/.zshrc
 # remove old i3 config files replace with new
 rm ~/.config/i3/*
 ln -s $FOLDER/.config/i3/* ~/.config/i3/
+rm ~/.i3blocks.conf
+ln -s $FOLDER/.i3blocks.conf ~/.i3blocks.conf
 
 # copy misc files
 ln -s $FOLDER/images/wallpaper.jpg ~/.config/i3/
