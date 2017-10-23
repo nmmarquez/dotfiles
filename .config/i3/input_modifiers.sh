764160299c5b3691911f94235a4a4d369b2c1cef
Neal Marquez <nmmarquez@gmail.com>
2017-10-23 07:03:04 -0700

    update touchpad scroll script

--- .config/i3/input_modifiers.sh
+++ .config/i3/input_modifiers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TOUCH=$(xinput | grep "TouchPad" | awk '{print $6}' | tr -dc '0-9')
+TOUCH=$(xinput | grep "TouchPad" --ignore-case | awk '{print $6}' | tr -dc '0-9')
 TOUCH_ENTITY=$(xinput list-props $TOUCH | grep "Scrolling Distance" | awk '{print $ 4}' | tr -dc '0-9')
 xinput set-prop $TOUCH $TOUCH_ENTITY -99 -99
 
