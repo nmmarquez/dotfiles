126cd3a8a7c3d64998e9c756f898a966e4491d6d
Neal Marquez <nmarquez@uw.edu>
2018-11-14 11:26:55 -0800

    update install for ubuntu 18.04

--- install/media_install.sh
+++ install/media_install.sh
@@ -1,16 +1,23 @@
 # Install Spotify  
-sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list'
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-sudo apt-get update
-sudo apt-get install spotify-client
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install spotify-client
 
 # Install Docker Instance
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo usermod -aG docker ${USER}
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker # sanity check to see if running
+
+# add regular user to docker group
+sudo usermod -aG docker $USER
 
 # Install mpv
-sudo apt-get install mpv
+sudo apt install mpv
 
 
